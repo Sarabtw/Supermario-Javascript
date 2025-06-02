f3db8c892f1d9fb8be36352f0b1d6ded8fa520f7
Larissa de Oliveira <larissaoliveira3zb@gmail.com>
2025-06-02 13:26:14 -0300

    Adicionando Score

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
-
+const scoreElement = document.getElementById('score');
+let score = 0;
 
 const jump = () => {
   mario.classList.add('jump');
@@ -12,30 +13,30 @@ const jump = () => {
 
 const loop = setInterval(() => {
 
-    
-
-   const pipePosition = pipe.offsetLeft;
-   const marioPosition = window.getComputedStyle(mario).bottom.replace('px', '');
 
 
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = window.getComputedStyle(mario).bottom.replace('px', '');
 
-   if (pipePosition <= 120 && pipePosition > 0 && marioPosition< 80){
 
-      pipe.style.animation = 'none';
-      pipe.style.left = `${pipePosition}px`;
 
-      mario.style.animation = 'none';
-      mario.style.bottom = `${marioPosition}px`;
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
 
-      mario.src = "img/game-over.png";
-      mario.style.width = '75px';
-      mario.style.marginLeft = '50px';
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
 
-      clearInterval(loop);
-   };
+    mario.src = "img/game-over.png";
+    mario.style.width = '75px';
+    mario.style.marginLeft = '50px';
 
-}, 10);
+    clearInterval(loop);
+  } else {
+
+    score++;
+    scoreElement.textContent = score;
+  }
+}, 100);
 
 document.addEventListener('keydown', jump);
-
-
